0a7c7b39769d9581288f12c6ab7a6381c70d0358
NeverDh <luisgabriel20000@outlook.com>
2023-02-03 20:13:36 -0300

    site pré-finalizado

--- js/index.js
+++ js/index.js
@@ -31,20 +31,10 @@ function nav_list_click(){
         idElemento = e.target.id
         sessionStorage.setItem('id_nav', idElemento);
     });
+    alterar_nav()
 }
 
-window.onload = function alterar_nav(){
-    loadStorageNav = sessionStorage.getItem('id_nav');
-
-    for(i =1; i<=5; i++){
-        let navs = document.getElementById("nav" + i)
-        
-        if(navs.id == loadStorageNav){
-            navs.style.color='#FFD700'
-            navs.style.transition="0s"
-            break
-        }
-    }
+window.onload = function() {
 
     
 
@@ -61,6 +51,35 @@ window.onload = function alterar_nav(){
     setTimeout(animaWpp, 2000)
 }
 
+// function alterar_nav(){
+//     loadStorageNav = sessionStorage.getItem('id_nav');
+//     let navOne = document.getElementById("nav1")
+//     let navTwo = document.getElementById("nav2")
+//     let navThree = document.getElementById("nav3")
+//     let navFour = document.getElementById("nav4")
+//     let navFive = document.getElementById("nav5")
+//     navOne.style.color='#FFFFFF'
+//     navOne.style.transition="1s"
+//     navTwo.style.color='#FFFFFF'
+//     navTwo.style.transition="1s"
+//     navThree.style.color='#FFFFFF'
+//     navThree.style.transition="1s"
+//     navFour.style.color='#FFFFFF'
+//     navFour.style.transition="1s"
+//     navFive.style.color='#FFFFFF'
+//     navFive.style.transition="1s"
+
+    
+//     for(i =1; i<=5; i++){
+//         let navs = document.getElementById("nav" + i)
+        
+//         if(navs.id == loadStorageNav){
+//             navs.style.color='#FFD700'
+//             navs.style.transition="1s"
+//             break
+//         }
+//     }
+// }
 
 // function hide (){
 //     navbar[0].style.position="absolute"
@@ -212,3 +231,92 @@ function animaWpp(){
 }
 
 // -------------------------------------FIM ANIMAÇÃO WHATSAPP--------------------------------- //
+
+function teste(){
+    var sobre = document.getElementById("sobre")
+    var aplicabilidade = document.getElementById("aplicabilidade")
+    var processos = document.getElementById("processos")
+    var atuação = document.getElementById("atuação")
+    var contato = document.getElementById("contato")
+    var scrollYSobre = sobre.getBoundingClientRect()
+    var scrollYAplicabilidade = aplicabilidade.getBoundingClientRect()
+    var scrollYProcessos = processos.getBoundingClientRect()
+    var scrollYAtuação = atuação.getBoundingClientRect()
+    var scrollYContato = contato.getBoundingClientRect()
+    let navOne = document.getElementById("nav1")
+    let navTwo = document.getElementById("nav2")
+    let navThree = document.getElementById("nav3")
+    let navFour = document.getElementById("nav4")
+    let navFive = document.getElementById("nav5")
+    // var convertPx2 = {
+    //     vh: function(px){
+    //       px = parseFloat(px);
+    //       var wh = $(window).height(); // captura a altura da viewport
+      
+    //       // faz a relação de  porcentagem dos pixels informados
+    //       // com a altura da viewport
+    //       return (px*100)/wh;
+    //     }
+    // }
+
+    var divOne = document.querySelector(".One")
+    var alturaOne = parseInt(divOne.clientHeight, 10)
+
+    var divTwo = document.querySelector(".Two")
+    var alturaTwo = parseInt(divTwo.clientHeight, 10)
+
+    var divThree = document.querySelector(".Three")
+    var alturaThree = parseInt(divThree.clientHeight, 10)
+
+    var divFour = document.querySelector(".Four")
+    var alturaFour = parseInt(divFour.clientHeight, 10)
+
+    var footer = document.querySelector("#contato")
+    var alturaFooter = parseInt(footer.clientHeight, 10)
+
+    if(scrollYSobre.y <= 100 + 500  && scrollYSobre.y >= alturaOne * (-1)){
+        navOne.style.color='#FFD700'
+        navOne.style.transition="all 1s"
+    }
+    else{
+        navOne.style.color='#FFFFFF'
+        navOne.style.transition="1s"
+    }
+
+    if(scrollYAtuação.y <= 0 && scrollYAtuação.y >= alturaTwo * (-1) - 100){
+        navTwo.style.color='#FFD700'
+        navTwo.style.transition="all 1s"
+    }
+    else{
+        navTwo.style.color='#FFFFFF'
+        navTwo.style.transition="all 1s"
+    }
+    if(scrollYProcessos.y <= 0 && scrollYProcessos.y >= alturaThree * (-1)){
+        navThree.style.color='#FFD700'
+        navThree.style.transition="all 1s"
+    }
+    else{
+        navThree.style.color='#FFFFFF'
+        navThree.style.transition="all 1s"
+    }
+    if(scrollYAplicabilidade.y <= 0 && scrollYAplicabilidade.y >= alturaFour * (-1) + 200){
+        navFour.style.color='#FFD700'
+        navFour.style.transition="all 1s"
+    }
+    else{
+        navFour.style.color='#FFFFFF'
+        navFour.style.transition="all 1s"
+    }
+    if(scrollYContato.y <= alturaFour + 100 && scrollYContato.y >= (alturaFooter * (-1))){
+        navFive.style.color='#FFD700'
+        navFive.style.transition="all 1s"
+    }
+    else{
+        navFive.style.color='#FFFFFF'
+        navFive.style.transition="all 1s"
+    }
+}
+
+window.addEventListener('scroll', function(){
+    teste();
+})
